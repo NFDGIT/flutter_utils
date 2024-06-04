e742777acb3f27febe515baf86251d9ff95a6e81
NFDGIT <NFDGIT@users.noreply.github.com>
2024-06-04 14:56:47 +0800

    Update flutter_utils.dart
    添加函数的防抖动和阀门机制

--- lib/flutter_utils.dart
+++ lib/flutter_utils.dart
@@ -9,9 +9,55 @@ import 'package:flutter/foundation.dart';
 
 import 'flutter_utils_platform_interface.dart';
 
+import 'dart:async';
+
+
+
+
 class FlutterUtils {
   Future<String?> getPlatformVersion() {
     return FlutterUtilsPlatform.instance.getPlatformVersion();
+  }
+}
+
+/// 延迟防抖动
+extension DebounceExtension<T, R> on Function(T) {
+  Function(T) debounce(Duration duration) {
+    Timer? timer;
+    T? lastArgs;
+
+    return (T args) {
+      lastArgs = args;
+      if (timer != null) {
+        timer!.cancel();
+      }
+      timer = Timer(duration, () {
+        this(lastArgs as T);
+      });
+    };
+  }
+}
+/// 防止多次点击
+extension ThrottleExtension<T, R> on Function(T) {
+  Function(T) throttle(Duration duration) {
+    late T? lastArgs;
+    late bool isThrottled = false;
+
+    return (T args) {
+      if (isThrottled) {
+        lastArgs = args;
+        return;
+      }
+      isThrottled = true;
+      this(args);
+      Timer(duration, () {
+        isThrottled = false;
+        if (lastArgs != null) {
+          this(lastArgs!);
+          lastArgs = null;
+        }
+      });
+    };
   }
 }
 
